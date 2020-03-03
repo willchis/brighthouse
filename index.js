586d8eda68ec02040b93d2e40f76d7e346119188
Will Chislett <will.chislett@teamlewis.com>
2020-03-02 21:03:05 -0800

    removing old code.

--- index.js
+++ index.js
@@ -4,44 +4,15 @@ const chromeLauncher = require('chrome-launcher');
 const express = require('express');
 const http = require('http')
 const path = require('path');
-const socketIo = require('socket.io');
-
-const MAX_MESSAGE_HISTORY = 1000;
 
 const app = express();
 const server = http.Server(app);
-const io = socketIo(server);
-
 let history = [];
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public', 'index.html'));
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.emit('history', history);
-
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-        
-    });
-
-    socket.on('chat message', (msg) => {
-        if (msg) {
-            console.log('message: ' + msg);
-            io.emit('chat message', msg);
-            
-            if (history.length > MAX_MESSAGE_HISTORY) {
-              history = []
-            }
-
-            history.push(msg);
-            console.log(history.toString());
-        }
-    });
 });
 
 const port = process.env.PORT || 5000;
