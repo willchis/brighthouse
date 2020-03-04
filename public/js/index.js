b93b49e3473174a46ea7f31bf099aeea74c58742
willchis <willchis@hotmail.com>
2020-03-03 22:36:11 -0800

    Animated percentage icons.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,8 @@
 $(() => {
+	
+
+	
+
 	$('form').submit(function(event){
 	    event.preventDefault(); //prevent default action 
 	    $("#results").html('');
@@ -22,10 +26,37 @@ $(() => {
 	    				score = score.toString().substring(2);
 	    			}
 	    			let percent = score.indexOf("error") == -1 ? '%' : '';
-	    			$("#results").append(`<div class="col text-center">${property}: <div class="col">${score}${percent}</div>`)
+					$("#results").append(`
+					<div class="col text-center">${property}
+					<div class="c100 p0 big" id="div_loading_progress_${property}"><span id="span_progress_${property}">0%</span>
+					<div class="slice">
+						<div class="bar"></div>
+						<div class="fill"></div>
+					</div>
+					</div>
+					</div>`);
+					let max = score;
+					let pct = 0;
+					let span_progress = document.getElementById("span_progress_" + property);
+					let div_loading_progress = document.getElementById("div_loading_progress_" + property);
+
+					function display_pct(p) {
+						span_progress.innerHTML="" + p + "%";
+						div_loading_progress.className="c100 p"+p;
+					}
+
+					function update_pct(){
+						display_pct(pct++);
+								
+						if (pct <= max) {
+							setTimeout(update_pct, 10);
+						}
+					}
+					setTimeout(update_pct, 50);
 				}
 	    	}
 	    });
 	});
+
 
 });
